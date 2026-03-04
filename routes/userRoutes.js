39486a4c5f2701aaf6209eae3d3db434760de6fe
Athisankar-2006 <athisankar2k6@gmail.com>
2026-03-04 22:02:03 +0530

    code completed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,29 @@
-// const express =require("express");
-// const User=require("../models/user");
-// const protect=require("../middleware/authMiddleware");
+const express =require("express");
+const User=require("../models/user");
+const protect=require("../middleware/authMiddleware");
 
 
-// const router=express.Router();
+const router=express.Router();
 
-// // get profile
+// get profile
 
-// router.get("/profile",protect,async(requestAnimationFrame,res)=>{
+router.get("/profile",protect,async(req,res)=>{
+    const user =await User.findById(req,user).select("-password");
+    res.json(user);
 
-// })
+});
+
+//update profile
+router.put("/profile",protect,async(req,res)=>{
+    const user=await User.findById(req.user);
+
+    if(user){
+        user.name=req.body.name || user.name;
+        user.email=req.body.email || user.email;
+
+        await user.save();
+        res.json({ message: "profile updated"});
+    }
+});
+
+module.exports=router;
--- server.js
+++ server.js
@@ -2,19 +2,24 @@
 require("dotenv").config();
 const express=require("express");
 const connectDB=require("./config/db");
-// const mongoose=require("mongoose");
 
-// mongoose.connect(process.env.MONGO_URL);
+
+const authRoutes=require("./routes/authRoutes");
+const userRoutes=require("./routes/userRoutes");
 
 connectDB();
 
 
 const app=express();
+app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("hellow this is for check");
-    console.log("enjoy every moment");
-})
+app.use("/api/auth",authRoutes);
+app.use("/api/user",userRoutes);
+
+// app.get("/",(req,res)=>{
+//     res.send("hellow this is for check");
+//     console.log("enjoy every moment");
+// })
 
 
 
